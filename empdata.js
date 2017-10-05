c08f32276758e55a8230058d4d1c16181b2d36b3
Grace Jin <jinm@seattleu.edu>
2017-10-04 17:01:36 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -1,124 +1,89 @@
 {
   "data": [
     {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "name": "Apple Bee",
+      "phone number": "206-123-4567",
+      "email address": "beea@seattleu.edu"
+      
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "name": "Brain Li",
+      "phone number": "425-321-0987",
+      "email address": "lib2@seattleu.edu"
+      
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "name": "Chole Smith",
+      "phone number": "253-098-4567",
+      "email address": "smithc14@seattleu.edu"
+      
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "name": "Dan Woods",
+      "phone number": "360-345-6789",
+      "email address": "woodsd@seattleu.edu"
+     
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "name": "Ellen More",
+      "phone number": "206-123-4568",
+      "email address": "moree1@seattleu.edu"
+     
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "name": "Frank Zhao",
+      "phone number": "425-321-0988",
+      "email address": "zhaof@seattleu.edu"
+      
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "name": "Gaoming Wu",
+      "phone number": "253-098-4568",
+      "emial address": "wug8@seattleu.edu"
+    
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "name": "Hanna Kim",
+      "phone number": "360-345-6790",
+      "email address": "kimh@seattleu.edu"
+    
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "name": "Ivy Su",
+      "phone number": "206-123-4569",
+      "emial address": "sui@seattleu.edu"
+      
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "name": "Jack Atwood",
+      "phone number": "425-321-0989",
+      "email address": "atwoodj@seattleu.edu"
+     
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "name": "Kira Chan",
+      "phone number": "253-098-4569",
+      "email address": "chank@seattleu.edu"
+      
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "name": "Lizzy War",
+      "phone number": "360-345-6791",
+      "email address": "warl@seattleu.edu"
+      
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "name": "Miaoming He",
+      "phone number": "206-123-4570",
+      "email address": "hemia@seattleu.edu"
+      
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "name": "Nick Pham",
+      "phone number": "425-321-0990",
+      "email address": "phamn@seattleu.edu"
+      
+  
     }
   ]
 }
